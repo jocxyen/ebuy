06abb4c8cc6793acfaff9356ec1b0c2d0aa5b4c9
Jocxyen <jocxyen@gmail.com>
2022-11-15 22:41:56 +0800

    Edit TailwindCSS

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains:["fakestoreapi.com", "cdn.shopify.com", "api.lorem.space"]
+  }
 }
 
 module.exports = nextConfig
