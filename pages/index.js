a6450e14f45e2116934397aa6b1cdcb9237bbdd2
Jocxyen <jocxyen@gmail.com>
2022-11-18 23:00:57 +0800

    Edit localstorage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { Banner, Header, ProductList, ReviewContainer } from '../components'
-import { fetchCart } from '../store/cartSlice'
 
 export async function getStaticProps() {
   const men = await fetch("https://fakestoreapi.com/products/category/men's%20clothing")
@@ -21,12 +18,7 @@ export async function getStaticProps() {
     }
 }
 export default function Home({ products }) {
-  const dispatch = useDispatch()
-  const { cartItems } = useSelector(state  => state.cart);
-  useEffect(() => {
-    dispatch(fetchCart(JSON.parse(localStorage?.getItem('cart') || [])))
-    console.log("run")
-  },[dispatch])
+
   return (
     <div className="flex flex-col w-full h-full">
       <Head>
