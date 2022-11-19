956ae689ad3cb7124b8dff34b33b309fd520c81b
Jocxyen <jocxyen@gmail.com>
2022-11-19 14:32:33 +0800

    Firebase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDtx1J9XRSkoMPlGxHeCFji6JBtyzXGH0c",
+  authDomain: "test-26a3d.firebaseapp.com",
+  projectId: "test-26a3d",
+  storageBucket: "test-26a3d.appspot.com",
+  messagingSenderId: "827420827057",
+  appId: "1:827420827057:web:ba10542d4945a5533a3190"
+};
